3d0315f209aded0c8d72aae686cc0b72506e6bd5
Vadim Peretokin <vperetokin@gmail.com>
2021-02-09 11:38:09 +0100

    Configure it as well if ASPNETCORE_FORWARDEDHEADERS_ENABLED is on

--- Vonk.Plugin.UseForwardHeadersMiddleware/VonkPluginConfiguration.cs
+++ Vonk.Plugin.UseForwardHeadersMiddleware/VonkPluginConfiguration.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.DependencyInjection;
 using Vonk.Core.Pluggability;
@@ -7,10 +8,25 @@ namespace Vonk.Plugin.UseForwardHeadersMiddleware {
     [VonkConfiguration (order: 900)] // Needs to be configured before the HttpToVonkConfiguration
     public class UseForwardHeadersMiddlewareConfiguration {
         public static IServiceCollection ConfigureServices (IServiceCollection services) {
+            if (string.Equals (
+                    Environment.GetEnvironmentVariable ("ASPNETCORE_FORWARDEDHEADERS_ENABLED"),
+                    "true", StringComparison.OrdinalIgnoreCase)) {
+                services.Configure<ForwardedHeadersOptions> (options => {
+                    options.ForwardedHeaders = ForwardedHeaders.XForwardedFor |
+                        ForwardedHeaders.XForwardedProto;
+                    // Only loopback proxies are allowed by default.
+                    // Clear that restriction because forwarders are enabled by explicit
+                    // configuration.
+                    options.KnownNetworks.Clear ();
+                    options.KnownProxies.Clear ();
+                });
+            }
+
             return services;
         }
 
         public static IApplicationBuilder Configure (IApplicationBuilder builder) {
+
             builder.UseMiddleware<ForwardedHeadersMiddleware> ();
             return builder;
         }
